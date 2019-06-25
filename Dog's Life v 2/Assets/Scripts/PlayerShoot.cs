221beaa75952c15c61aa57c3ece7a6cca6bfa86c
Pyrozen <william.oky@binus.ac.id>
2019-06-25 08:50:31 +0700

    Adding Shoot Logic

---
+++ Dog's Life v 2/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+    [SerializeField]
+    private LayerMask mask;
+    private float range = 5.0f;
+    void Start()
+    {
+        Debug.Log("Start");
+        if(cam==null)
+        {
+            Debug.Log("Error, No Camera Detected!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Shoot");
+            Shoot();
+        }
+        
+    }
+    void Shoot()
+    {
+        RaycastHit _hit;
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit,range, mask))
+        {
+            string objectName = _hit.collider.name;
+            Debug.Log(_hit.collider.name);
+            if(objectName.Contains("Cat"))
+            {
+                Debug.Log("Cat Found!");
+                Destroy(_hit.transform.gameObject);
+            }
+        }
+    }
+}
